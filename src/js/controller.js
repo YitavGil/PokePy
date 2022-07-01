344f17e95e52cde3d65c6b5b038d73c2b61fec41
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-07-01 18:18:22 +0300

    Add files via upload

--- src/js/controller.js
+++ src/js/controller.js
@@ -36,15 +36,16 @@ const getPokemonsList = async function () {
       <svg class="recipe__info-icon">
         <use href="src/img/icons.svg#icon-clock"></use>
       </svg>
+      <span class="recipe__info-text">Type: &nbsp;</span>
       <span class="recipe__info-data recipe__info-data--minutes">${data.types[0].type.name}</span>
-      <span class="recipe__info-text">minutes</span>
     </div>
     <div class="recipe__info">
       <svg class="recipe__info-icon">
         <use href="src/img/icons.svg#icon-users"></use>
       </svg>
+      <span class="recipe__info-text">Available Moves:&nbsp; </span>
       <span class="recipe__info-data recipe__info-data--people">${data.moves.length}</span>
-      <span class="recipe__info-text">servings</span>
+      
 
       <div class="recipe__info-buttons">
         <button class="btn--tiny btn--increase-servings">
@@ -75,6 +76,7 @@ const getPokemonsList = async function () {
   <div class="recipe__ingredients">
     <h2 class="heading--2">Recipe ingredients</h2>
     <ul class="recipe__ingredient-list">
+    
       <li class="recipe__ingredient">
         <svg class="recipe__icon">
           <use href="src/img/icons.svg#icon-check"></use>
@@ -118,6 +120,7 @@ const getPokemonsList = async function () {
     </a>
   </div>
     `;
+    recipeContainer.innerHTML = '';
     recipeContainer.insertAdjacentHTML('afterbegin', markup)
   } catch (error) {
     console.log(error);
